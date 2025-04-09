860f2f0ec52b214687d6b763a4f7c59e62a7b54a
Yutaka Hirano <hirano.y.aa@gmail.com>
2025-04-09 05:33:53 +0000

    Calculate complementary gap on the surface code
    The file surface-code.stim is not included in this commit due to
    uncertainty about its licensing.

---
+++ complementary-gap/main.py
@@ -0,0 +1,46 @@
+import numpy as np
+import pymatching
+import stim
+
+
+def main() -> None:
+    num_shots = 1000
+    circuit: stim.Circuit
+    with open('surface-code.stim') as f:
+        circuit = stim.Circuit(f.read())
+    dem = circuit.detector_error_model(decompose_errors=True)
+    matcher = pymatching.Matching.from_detector_error_model(dem)
+    sampler = circuit.compile_detector_sampler()
+
+    detection_events, observable_flips = sampler.sample(num_shots, separate_observables=True)
+    all_edges = matcher.edges()
+
+    for shot in range(num_shots):
+        syndrome = detection_events[shot]
+        weight = 0
+        for [x, y] in matcher.decode_to_edges_array(syndrome):
+            y = None if y == -1 else y
+            ls = [attr['weight'] for (u, v, attr) in all_edges if (u == x and v == y) or (u == y and v == x)]
+            assert len(ls) == 1
+            weight += ls[0]
+
+        complementary_weight = 0
+
+        # Flip the last detector.
+        syndrome[-1] = 1 - syndrome[-1]
+        edges = matcher.decode_to_edges_array(syndrome)
+        syndrome[-1] = 1 - syndrome[-1]
+
+        for [x, y] in edges:
+            y = None if y == -1 else y
+            ls = [attr['weight'] for (u, v, attr) in all_edges if (u == x and v == y) or (u == y and v == x)]
+            assert len(ls) == 1
+            complementary_weight += ls[0]
+
+        gap = complementary_weight - weight
+        print('weight = {:8.3f}, complementary_weight = {:8.3f}, gap = {:8.3f}'.format(
+            weight, complementary_weight, gap))
+
+
+if __name__ == '__main__':
+    main()
