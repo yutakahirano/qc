94f6e746b8efdf85349ff62a0b019f25c672526c
Yutaka Hirano <hirano.y.aa@gmail.com>
2025-04-10 03:47:24 +0000

    [COMPLEMENTARY GAP] Remove `calculate_weight`
    We can get the information with the `return_weight` parameter to
    pymatching.Matching.decode.

--- complementary-gap/main.py
+++ complementary-gap/main.py
@@ -1,16 +1,5 @@
-import numpy as np
 import pymatching
 import stim
-
-
-def calculate_weight(syndrome: np.ndarray, matcher: pymatching.Matching) -> float:
-    all_edges = matcher.edges()
-    weight = 0
-    for [x, y] in matcher.decode_to_edges_array(syndrome):
-        y = None if y == -1 else y
-        [w] = [attr['weight'] for (u, v, attr) in all_edges if (u, v) == (x, y) or (u, v) == (y, x)]
-        weight += w
-    return weight
 
 
 def main() -> None:
@@ -26,11 +15,11 @@ def main() -> None:
 
     for shot in range(num_shots):
         syndrome = detection_events[shot]
-        weight = calculate_weight(syndrome, matcher)
+        _, weight = matcher.decode(syndrome, return_weight=True)
 
-        syndrome[-1] = 1 - syndrome[-1]
-        complementary_weight = calculate_weight(syndrome, matcher)
-        syndrome[-1] = 1 - syndrome[-1]
+        syndrome[-1] = not syndrome[-1]
+        _, complementary_weight = matcher.decode(syndrome, return_weight=True)
+        syndrome[-1] = not syndrome[-1]
 
         gap = complementary_weight - weight
         print('weight = {:8.3f}, complementary_weight = {:8.3f}, gap = {:8.3f}'.format(
